8185d6346da201f5c3c8696aaa7ead1effe16668
kroles1 <100869848+kroles1@users.noreply.github.com>
2022-05-12 18:57:32 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
-const bloom = document.getElementById('bloom');
+document.addEventListener('DOMContentLoaded', function() {
+    const bloom = document.getElementById('bloom');
 
 bloom.addEventListener('mouseover', function handleMouseOver() {
     bloom.style.color='yellow';
@@ -6,4 +7,5 @@ bloom.addEventListener('mouseover', function handleMouseOver() {
 
 bloom.addEventListener('mouseout', function handleMouseOut() {
     bloom.style.color='black';
+});
 });
